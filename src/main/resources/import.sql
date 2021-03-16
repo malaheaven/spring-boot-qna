8a2949066fa964f29e8a7f418fec37aafd021aa2
malaheaven <yunhg95@gmail.com>
2021-03-16 11:57:24 +0900

    feat: 매번 유저 데이터를 넣는 부분이 반복되어 그 부분을 해결하기 위해 INSERT 쿼리 파일 삽입

---
+++ src/main/resources/import.sql
@@ -0,0 +1,3 @@
+INSERT INTO USER(USER_ID, PASSWORD, NAME, EMAIL) VALUES ('robin', '1234', 'robin2', 'robin@gmail.com');
+INSERT INTO USER(USER_ID, PASSWORD, NAME, EMAIL) VALUES ('test', '1234', 'robintest', 'robintest@gmail.com');
+INSERT INTO USER(USER_ID, PASSWORD, NAME, EMAIL) VALUES ('test22', '1234', 'test22', 'test22@gmail.com');
